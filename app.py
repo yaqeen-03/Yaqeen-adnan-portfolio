5817e0b6cb3b697fd317b875c5ab7e3f49c5af6a
yaqeen-03 <162414876+yaqeen-03@users.noreply.github.com>
2024-03-07 11:32:05 +0000

    page with style
    page with style

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app=Flask(__name__)
 @app.route('/')
 def hello_world():
-  return "Hello, yaqeen!"
+  return render_template('home.html')
 
 
 
